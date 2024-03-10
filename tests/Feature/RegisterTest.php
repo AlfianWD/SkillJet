b9127eb2a2b14b21fd504d81d9f89af348353b25
AlfianWD <carzyman01@gmail.com>
2024-03-11 00:23:37 +0700

    [test] : add Feature test in packages

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    /**
+     * A basic test register.
+     */
+    public function it_renders_corrently()
+    {
+        $this->assertTrue(true);
+    }
+}
